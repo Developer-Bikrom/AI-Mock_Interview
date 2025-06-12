2db8f8978674c61f774d3a11e4415a9040c00388
Bickrom Mitra <bickrommitra@gmail.com>
2025-06-12 10:01:14 +0530

    Changes:projects Initiated &  Setup Completed with Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+ 
+
+function App() {
+  return (
+    <div className='text-blue-500'>
+      <Button >click</Button>
+    </div>
+  )
+}
+
+export default App
