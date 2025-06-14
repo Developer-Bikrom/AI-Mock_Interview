4ebe86b4e0643ee8201d35878c49c6ac9d96553a
Bickrom Mitra <bickrommitra@gmail.com>
2025-06-14 14:03:49 +0530

    Changes:Clerk Authentication & Routing Setup Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { Button } from "@/components/ui/button"
-
- 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PublicLayout } from "@/layouts/public-layout";
+import HomePage from "@/routes/home";
+import AuthenticationLayout from "@/layouts/auth-layout";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+import ProtectRoutes from "./layouts/protected-layout";
+import { MainLayout } from "./layouts/main-layout";
 
 function App() {
   return (
-    <div className='text-blue-500'>
-      <Button >click</Button>
-    </div>
+    <Router>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<PublicLayout/>}>
+          <Route index element={<HomePage/>}/>
+        </Route>
+
+        {/* Authentication Layout*/}
+
+         <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage/>} />
+          <Route path="/signup/*" element={<SignUpPage/>} />
+        </Route>
+
+
+        {/* protected routes */}
+        <Route 
+          element={
+           <ProtectRoutes>
+             <MainLayout/>
+              </ProtectRoutes>
+            }>
+          {/* add all protected routes */}
+        </Route>
+
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ src/routes/home.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/clerk-react"
+
+
+const HomePage = () => {
+  return (
+    <div>
+      Home Page <UserButton/>
+    </div>
+  )
+}
+
+export default HomePage
