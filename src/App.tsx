31e6a6d4d4404ad808b9534b142539911525fcd7
Bickrom Mitra <bickrommitra@gmail.com>
2025-06-19 12:31:35 +0530

    Changes:Project completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,52 @@ import { SignInPage } from "./routes/sign-in";
 import { SignUpPage } from "./routes/sign-up";
 import ProtectRoutes from "./layouts/protected-layout";
 import { MainLayout } from "./layouts/main-layout";
+import { Generate } from "./components/generate";
+import { Dashboard } from "./routes/dashboard";
+import { CreateEditPage } from "./routes/create-edit-page";
+import { MockLoadPage } from "./routes/mock-load-page";
+import { MockInterviewPage } from "./routes/mock-interview-page";
+import { Feedback } from "./routes/feedback";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/*public routes*/}
-        <Route element={<PublicLayout/>}>
-          <Route index element={<HomePage/>}/>
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
         </Route>
 
         {/* Authentication Layout*/}
 
-         <Route element={<AuthenticationLayout />}>
-          <Route path="/signin/*" element={<SignInPage/>} />
-          <Route path="/signup/*" element={<SignUpPage/>} />
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
         </Route>
-
 
         {/* protected routes */}
-        <Route 
+        <Route
           element={
-           <ProtectRoutes>
-             <MainLayout/>
-              </ProtectRoutes>
-            }>
+            <ProtectRoutes>
+              <MainLayout />
+            </ProtectRoutes>
+          }
+        >
           {/* add all protected routes */}
+          <Route element={<Generate />} path="/generate">
+            <Route index element={<Dashboard />} />
+            <Route path=":interviewID" element={<CreateEditPage/>}/>
+            <Route path="interview/interviewId" element={<MockLoadPage/>}/>
+            <Route
+              path="interview/:interviewId" 
+              element={<MockInterviewPage/>}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
+          </Route>
         </Route>
-
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
